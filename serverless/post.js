e1a9b780b45d022705dd505f2fa92ea6a59dedf8
hx-w <herixth@163.com>
2021-11-24 14:51:16 +0800

    test

--- serverless/post.js
+++ serverless/post.js
@@ -1,6 +1,21 @@
-exports.handler = async function(event, context) {
+const redis = require('redis')
+exports.handler = async function (event, context) {
+    const redisClient = redis.createClient(27014, 'mc.csgowiki.top')
+    redisClient.on('error', err => {
+        console.error(err)
+    })
+    redisClient.auth(process.env.REDIS_PASSWORD)
+    client.set("test", event.body, function (error, res) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(res);
+        };
+    });
+    client.end(true);
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World" + process.env.REDIS_PASSWORD})
+        body: JSON.stringify({ message: "Hello World" + event.body })
     };
 }
--- src/views/auto-checkin.vue
+++ src/views/auto-checkin.vue
@@ -175,13 +175,6 @@ export default {
         "如果你需要每日打卡消息回执，请主动添加QQ(机器人)：<strong><a href='https://wpa.qq.com/msgrd?v=3&uin=3583618673&site=scubot&from=scubot&menu=yes'>3583618673</a></strong>，验证消息填：<strong><span id='verifyCode' onclick='copy();'>7355608</span></strong>",
       duration: 0,
     });
-    // this.$notify.info({
-    //   title: "功能相关",
-    //   message:
-    //     "地图定位功能还未接入，目前打卡定位地点为[四川大学望江校区研究生院]",
-    //   duration: 0,
-    //   offset: 120,
-    // });
   },
   methods: {
     onPreview() {
@@ -236,7 +229,8 @@ export default {
       var postData = JSON.parse(JSON.stringify(this.preview));
       postData.accessToken = this.accessToken;
       axios
-        .post("https://ci.scubot.live:12121/set_checkin", postData)
+        // .post("https://ci.scubot.live:12121/set_checkin", postData)
+        .post("https://www.scubot.live/.netlify/functions/post", postData)
         .then((res) => {
           if (res.status == 200) {
             this.$message.success(res.data["message"]);
