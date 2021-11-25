16f7624184f293ff12ed457f4ce717be1d825f00
hx-w <herixth@163.com>
2021-11-25 23:12:35 +0800

    success str 2 map

--- serverless/post/main.go
+++ serverless/post/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -42,18 +43,18 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	if initClient() != nil {
 		return Resp(403, "{\"detail\": \"数据库连接错误\"}")
 	}
-	var reqJson map[string]string
+	var reqJson map[string]interface{}
     if err := json.Unmarshal([]byte(request.Body), &reqJson); err != nil {
 		return Resp(403, "{\"detail\": \"请求体错误\"")
     }
-	content := reqJson["content"]
-	uid := reqJson["uid"]
-	err := rdb.Set(SCUBotKey(uid), content, 0).Err()
+	content, _ := json.Marshal(reqJson["content"])
+	uid := strconv.Itoa(int(reqJson["uid"].(float64)))
+	err := rdb.Set(SCUBotKey(uid), string(content), 0).Err()
     if err != nil {
 		return Resp(403, "{\"detail\": \"数据库操作错误\"}")
     }
 
-	return Resp(200, "{\"message\":" + content + "}")
+	return Resp(200, "{\"message\": \"操作成功\"}")
 }
 
 func main() {
