3eec0e83bb714b5d11d22c392c1ec290b4aa8403
hx-w <herixth@163.com>
2021-11-25 11:29:15 +0800

    a?

--- src/views/auto-checkin.vue
+++ src/views/auto-checkin.vue
@@ -197,6 +197,7 @@ export default {
             }
           })
           .then((resp) => {
+            console.log(resp)
             this.scu.uuid = JSON.parse(resp)["uid"];
             this.$message.success("cookies验证成功");
 
